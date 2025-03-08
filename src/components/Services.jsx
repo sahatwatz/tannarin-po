39a1009768868c48793235c7e363e1e9d3df5f54
sahatwatz <yoke.21@hotmail.com>
2025-03-08 22:45:23 +0700

    edit: rename comp

---
+++ src/components/Services.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Services = () => {
+  return (
+    <section className="bg-white h-128 w-screen flex justify-center items-center">
+      <section className="bg-amber-100 w-1/2 flex justify-center items-center gap-2">
+        <section className="bg-amber-500 h-96 w-1/4">
+          <h2>card 1</h2>
+        </section>
+        <section className="bg-amber-500 h-96 w-1/4">
+          <h2>card 2</h2>
+        </section>
+        <section className="bg-amber-500 h-96 w-1/4">
+          <h2>card 3</h2>
+        </section>
+        <section className="bg-amber-500 h-96 w-1/4">
+          <h2>card4</h2>
+        </section>
+      </section>
+    </section>
+  );
+};
+
+export default Services;
